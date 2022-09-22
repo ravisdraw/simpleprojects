2a8a51b095c38956722cd181cc982bfea34393dc
ravisdraw <ravikumarrk1118@gmail.com>
2022-09-22 15:57:39 +0530

    Expanding cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  panels: any;
+
+  ngOnInit(): void {
+    this.panels = document.querySelectorAll('.panel');
+    console.log(this.panels);
+
+    this.panels.forEach((panel: any) => {
+      panel.addEventListener('click', () => {
+        this.removeActiveClass();
+        panel.classList.add('active');
+      });
+    });
+  }
   title = 'simpleProjects';
+
+  removeActiveClass() {
+    this.panels.forEach((panel: any) => {
+      panel.classList.remove('active');
+    });
+  }
 }
